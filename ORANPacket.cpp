68930c0f76989a07ef84274479a8a3b627457377
mahmoud isa <mahmoudahalim0@gmail.com>
2024-09-28 05:08:29 +0300

    feat: Implement ORAN Packet

---
+++ ORANPacket.cpp
@@ -0,0 +1,77 @@
+#include "ORANPacket.hpp"
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+ORANPacket::ORANPacket(std::unordered_map<std::string, std::string> OranConfig)
+{
+    MaxNrb = std::stoi(OranConfig["MaxNrb"]);
+    NrbPerPacket = std::stoi(OranConfig["NrbPerPacket"]);
+    PayloadType = OranConfig["PayloadType"];
+    PayloadFile = OranConfig["Payload"];
+}
+
+ORANPacket::~ORANPacket()
+{
+}
+
+std::string ORANPacket::ReadPayloadFile()
+{
+    std::stringstream payload;
+    std::ifstream input(PayloadFile);
+    std::string sample;
+    while (input >> sample)
+    {
+        payload << std::hex << (std::stoi(sample) & 0xFF);
+    }
+    return payload.str();
+}
+
+std::vector<std::string> ORANPacket::GeneratePacket(int NumberOfFrames)
+{
+    std::vector<std::string> Packets;
+    std::stringstream packet;
+    unsigned int PacketsPerSymbol = std::ceil((double) MaxNrb / NrbPerPacket);
+    unsigned int PacketsPerSlot = PacketsPerSymbol * 14;
+    unsigned int PacketsPerSubframe = PacketsPerSlot * 2; // assume SCS = 30
+
+    unsigned int subframeId = 0;
+    unsigned int slotId = 0;
+    unsigned int symbolId = 0;
+    unsigned int startPrbu = 0;
+    std::string payload = ReadPayloadFile();
+    for (size_t i = 0; i < PacketsPerSubframe * NumberOfFrames * 10; i++)
+    {
+        packet << "00";
+        packet << "00"; // frameId
+        subframeId = i/ PacketsPerSubframe;
+        slotId = i / PacketsPerSlot;
+        symbolId = i / PacketsPerSymbol;
+
+        unsigned int octet11 = (subframeId << 4) + (slotId >> 2);
+        packet << std::hex << std::setw(2) << std::setfill('0') << (octet11 & 0xFF);
+
+        unsigned int octet12 = (slotId << 6) + symbolId;
+        packet << std::hex << std::setw(2) << std::setfill('0') << (octet12 & 0xFF);
+        packet << "00"; // octet13
+
+        unsigned int octet14 = startPrbu >> 8;
+        packet << std::hex << std::setw(2) << std::setfill('0') << (octet14 & 0x03);
+
+        unsigned int octet15 = startPrbu;
+        packet << std::hex << std::setw(2) << std::setfill('0') << (octet15 & 0xFF);
+
+        unsigned int octet16 = std::min(MaxNrb - startPrbu, NrbPerPacket);
+        startPrbu += NrbPerPacket;
+        startPrbu %= NrbPerPacket;
+
+        packet << std::hex << std::setw(2) << std::setfill('0') << (octet16 & 0xFF);
+        packet << payload.substr(i * NrbPerPacket, octet16);
+        Packets.push_back(packet.str());
+        packet.str("");
+        packet.clear();
+        }
+        return Packets;
+}
---
+++ ORANPacket.hpp
@@ -0,0 +1,23 @@
+#include <unordered_map>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#if !defined(ORAN_HPP)
+#define ORAN_HPP
+class ORANPacket
+{
+private:
+    int MaxNrb;
+    unsigned int NrbPerPacket;
+    std::string PayloadType;
+    std::string PayloadFile;
+    std::string ReadPayloadFile();
+
+public:
+    ORANPacket(std::unordered_map<std::string, std::string> OranConfig);
+    ~ORANPacket();
+    std::vector<std::string> GeneratePacket(int NumberOfFrames);
+};
+
+#endif // ORAN_HPP
